9b1423e85cf236ab58a49eb87497968c902cf357
GeniusJase <yuhacaranddalgona@naver.com>
2025-01-04 08:40:26 +0900

    Create demons.js

---
+++ demons.js
@@ -0,0 +1,5 @@
+const demons = [
+    { rank: 1, name: " seismic ii ", creator: "GREENo0o", verifier: "GREENo0o", HighestRanking: 1, Victor: "None", victors: "" },
+    { rank: 2, name: "zoa lol", creator: "WhaleDP", verifier: "WhaleDP", HighestRanking: 1, Victor: "ItzZoA", victors: ""},
+    { rank: 3, name: "fire wave", creator: "WhaleDP", verifier: "WhaleDP", HighestRanking: 3, Victor: "None", victors: ""}
+  ];
